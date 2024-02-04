cbb7464b79b660b339afb6a4ad148f94602b7e4d
lushepherd <143170142+lushepherd@users.noreply.github.com>
2024-02-04 18:47:38 +1000

    Added authentication

--- controllers/cards_controller.py
+++ controllers/cards_controller.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, jsonify, request, abort
 from main import db
 from models.cards import Card
+from models.users import User
 from schemas.card_schema import card_schema, cards_schema
 from datetime import date
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 cards = Blueprint('cards', __name__, url_prefix="/cards")
+
 
 # The GET routes endpoint
 @cards.route("/", methods=["GET"])
@@ -18,6 +21,7 @@ def get_cards():
 
 # The POST route endpoint
 @cards.route("/", methods=["POST"])
+@jwt_required()
 def create_card():
     # #Create a new card
     card_fields = card_schema.load(request.json)
@@ -38,17 +42,18 @@ def create_card():
 
 # Finally, we round out our CRUD resource with a DELETE method
 @cards.route("/<int:id>/", methods=["DELETE"])
+@jwt_required()
 def delete_card(id):
     #get the user id invoking get_jwt_identity
-    # user_id = get_jwt_identity()
-    # #Find it in the db
-    # user = User.query.get(user_id)
-    # #Make sure it is in the database
-    # if not user:
-    #     return abort(401, description="Invalid user")
-    # # Stop the request if the user is not an admin
-    # if not user.admin:
-    #     return abort(401, description="Unauthorised user")
+    user_id = get_jwt_identity()
+    #Find it in the db
+    user = User.query.get(user_id)
+    #Make sure it is in the database
+    if not user:
+        return abort(401, description="Invalid user")
+    # Stop the request if the user is not an admin
+    if not user.admin:
+        return abort(401, description="Unauthorised user")
     # find the card
     card = Card.query.filter_by(id=id).first()
     #return an error if the card doesn't exist
